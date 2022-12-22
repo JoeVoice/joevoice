425921842ad7f9570cf6ba154d7fce9c0de984f7
JoeVoice <121239968+JoeVoice@users.noreply.github.com>
2022-12-22 13:57:19 +0000

    Add files via upload
    A Text To Speech AI Assistant Written in Python
    Based on OpenAI's GPT-3 Language Model
    
    **DO NOT REMOVE API KEY OR THE PROGRAM WILL FAIL TO FUNCTION AS INTENDED**

---
+++ joevoice/joevoice.py
@@ -0,0 +1,32 @@
+import openai
+import pyttsx3
+
+# Replace YOUR_API_KEY with your actual OpenAI API key
+openai.api_key = "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"
+
+# Create a TTS engine using pyttsx3
+engine = pyttsx3.init()
+
+# Create a loop to continue the conversation until the user explicitly tells the program to exit
+while True:
+  # Prompt the user to enter a message
+  message = input("Enter a message (press Alt+X to exit): ")
+
+  # Check if the user wants to exit the program
+  if message.lower() == "alt+x":
+    break
+
+  # Use GPT-3 to generate a response to the user's message
+  response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=message,
+      max_tokens=128,
+      temperature=0.5,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+  ).get("choices")[0].get("text")
+
+  # Use the eSpeak program to speak the response
+  engine.say(response)
+  engine.runAndWait()
